9d569e72c8a9e274b231c758ee98e22ded9ae36f
ShinobuFormula <33146121+ShinobuFormula@users.noreply.github.com>
2022-04-03 18:33:48 +0200

    init gateway

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+const bodyParser = require('body-parser')
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const httpProxy = require('express-http-proxy')
+
+const serviceMonsterProxy = httpProxy(process.env.SERVICE_MENU_URI || 'http://localhost:3000');
+
+app.use(bodyParser.json())
+app.use(cookieParser());
+
+var corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 
-
-
-app.listen(3000, () => {
-    console.log("listening on port 3000");
+app.use('/', (req, res, next) => {
+    if (req.path.includes('monster')){
+        serviceMonsterProxy(req, res, next);
+    }
 });
+
+app.listen(3000)
