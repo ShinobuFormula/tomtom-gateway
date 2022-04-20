f28f53e461bbc5b8adf610fc5a27314769816408
ShinobuFormula <33146121+ShinobuFormula@users.noreply.github.com>
2022-04-20 18:54:23 +0200

    register & connect working and refactored

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+//mongodb
+const db = require('./db')
+//middleware
 const bodyParser = require('body-parser')
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
 const httpProxy = require('express-http-proxy')
-
+//micro-services
 const serviceMonsterProxy = httpProxy(process.env.SERVICE_MONSTER_URL || 'http://localhost:3000');
+const serviceFightProxy = httpProxy(process.env.SERVICE_FIGHT_URL || 'http://localhost:3000');
+//controllers
+const { connect, register } = require("./controller/access")
+
 
 app.use(bodyParser.json())
 app.use(cookieParser());
@@ -23,9 +30,23 @@ app.use(cors(corsOptions));
 //refresh token
 //Verif token before access route
 
+
+app.post('/register', async (req, res) => {
+    res.json(await register(req.body))
+})
+
+app.post('/connect', async (req, res) => {
+    res.json(await connect(req.body))
+})
+
 app.use('/', (req, res, next) => {
+
+
+
     if (req.path.includes('monster')){
         serviceMonsterProxy(req, res, next);
+    } else if (req.path.includes('fight')) {
+        serviceFightProxy(req, res, next);
     }
 });
 
--- controller/access.js
+++ controller/access.js
@@ -1,5 +1,5 @@
 const {createUser, getUserByEmail} = require('../model/user')
-const {createToken} = require('./token.controller')
+const {createToken} = require('./token')
 const bcrypt = require('bcryptjs');
 
 exports.connect = async (body) => {
@@ -14,14 +14,8 @@ exports.connect = async (body) => {
 }
 
 exports.register = async (body) => {
-    console.log(body)
-    return await bcrypt.hash(body.password, 8).then((hash) => {
-        let userData = body
-        //body["password"] = hash instead of let etc..
-        userData.password = hash
-        //double return???? check that
-        return createUser(userData).then( (user) => {
-            return user
-        })
-    })
+    const hash = await bcrypt.hash(body.password, 8)
+    body["password"] = hash
+    const newUser = await createUser(body)
+    return newUser
 }
